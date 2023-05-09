bbe77023e0e774587dd13f998f2c6ef4661908d8
Letícia Beatriz <83764695+let1ci4b@users.noreply.github.com>
2023-05-09 15:22:27 -0300

    Create atv2.kt

---
+++ PrecoIngressoCinema/atv2.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 101
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("Today, the movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    var price = 
+    if(age <= 12) 15;
+    else if(age in 13..60) if(isMonday == true) 25 else 30;
+    else if(age in 60..100) 20;
+    else -1;
+    return price;
+}
