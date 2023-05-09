d03293ebb286341bbd9cfba53630d75d312cdc20
Letícia Beatriz <83764695+let1ci4b@users.noreply.github.com>
2023-05-09 16:31:08 -0300

    Update atv2.kt

--- PrecoIngressoCinema/atv2.kt
+++ PrecoIngressoCinema/atv2.kt
@@ -11,10 +11,11 @@ fun main() {
     println("Today, the movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
     println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
 }
+
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
     return when (age) {
         in 0..12 -> 15
-        in 13..60 -> if(isMonday) 25 else 30
+        in 13..60 -> if (isMonday) 25 else 30
         in 60..100 -> 20
         else -> -1
 	}
