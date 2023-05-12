8559059a7e5cb008bfdca932feb9dc0e99370d61
Letícia Beatriz <83764695+let1ci4b@users.noreply.github.com>
2023-05-12 14:02:11 -0300

    Update atv4.kt

--- Musica/atv4.kt
+++ Musica/atv4.kt
@@ -1,16 +1,19 @@
+// Descrição: Classe para um app de reprodução de música
+
 fun main(){
     val musica = Musica("In the end", "Linkin Park", 2000, 5000000)
-    musica.printMusic()
-    
+    println(musica.printMusic())
+    println(musica.printFama())
 }
 
 class Musica
 (val titulo: String, val artista: String, val anolancamento: Int, val contReproducao: Int)
-{
-     val famosa : Boolean 
-     get() = contReproducao >= 1000
-     
-     fun printMusic() {
-         print("$titulo, de $artista, lançado em $anolancamento.")
+{   
+     fun printMusic() : String {
+         return("$titulo, de $artista, lançado em $anolancamento.")
     }
+     
+     fun printFama() : Boolean {
+         return(contReproducao >= 1000)
+     }
 }
