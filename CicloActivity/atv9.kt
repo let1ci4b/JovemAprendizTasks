bb9dbc7510104376db33c39e3c35bca2380a8de8
Letícia Beatriz <83764695+let1ci4b@users.noreply.github.com>
2023-05-23 17:03:44 -0300

    Create atv9.kt

---
+++ CicloActivity/atv9.kt
@@ -0,0 +1,50 @@
+// Descrição: Sobrescrição de cada parte do ciclo de vida de uma activity para observar os chamados.
+
+package com.example.ciclovida
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ciclovida.ui.theme.CicloVidaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this@MainActivity, "OnCreate!", Toast.LENGTH_SHORT).show()
+        Log.i("", "OnCreate!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this@MainActivity, "OnStart!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this@MainActivity, "OnResume!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity, "OnPause!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("", "OnStop!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("", "OnDestroy!")
+    }
+}
